ea8c41e7f26e4d729a07284592ef72d34ff00bec
Alex Ivanchyk <alexander.ivanchyk@gmail.com>
2024-01-30 13:41:47 +0200

    Update version in setup.py
    Version number in setup.py updated to 0.6.1. This update includes added new features to the translation functionality of the Django Restful Translator app, such as placeholder replacement and HTML entities unescaping.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_restful_translator',
-    version='0.6.0',
+    version='0.6.1',
     author='Alex Ivanchyk',
     author_email='alexander.ivanchik@gmail.com',
     description='A Django application providing translation functionalities for Django Rest Framework',
