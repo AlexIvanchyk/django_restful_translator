59685c2a1ee5bf9031a27cae4ee5c54cbceef275
Alex Ivanchyk <alexander.ivanchyk@gmail.com>
2024-03-28 13:59:35 +0200

    Changed type for AutoTranslatableDictField from Dict to Json

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_restful_translator',
-    version='0.9.3',
+    version='0.9.4',
     author='Alex Ivanchyk',
     author_email='alexander.ivanchik@gmail.com',
     description='A Django application providing translation functionalities for Django Rest Framework',
